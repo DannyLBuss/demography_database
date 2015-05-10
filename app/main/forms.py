anuppathak/flasky

Chapter 7: Large file structure (7a)

app/main/forms.py
@@ -1,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import TextField, SubmitField
+from wtforms.validators import Required
+
+
+class NameForm(Form):
+    name = TextField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')

DannyLBuss/demography_database

app/main/forms.py
@@ -1,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')