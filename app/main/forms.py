anuppathak/flasky

Chapter 13: Blog post comments (13a)

app/main/forms.py
@@ -51,3 +51,8 @@ class EditProfileAdminForm(Form):
 class PostForm(Form):
     body = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class CommentForm(Form):
+    body = TextField('Enter your comment', validators=[Required()])
+    submit = SubmitField('Submit')

DannyLBuss/demography_database

app/main/forms.py
@@ -53,3 +53,8 @@ class EditProfileAdminForm(Form):
 class PostForm(Form):
     body = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class CommentForm(Form):
+    body = StringField('Enter your comment', validators=[Required()])
+    submit = SubmitField('Submit')