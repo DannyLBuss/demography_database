86f7252a2d76cab6e268e919eab0e612796a52eb
Francesca <f.sargent@exeter.ac.uk>
2016-02-01 15:48:58 +0000

    Extending API

--- app/__init__.py
+++ app/__init__.py
@@ -44,6 +44,6 @@ def create_app(config_name):
     app.register_blueprint(conversion_blueprint, url_prefix='/conversion')
 
     from .api_1_0 import api as api_1_0_blueprint
-    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api')
 
     return app
--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request, current_app, url_for
 from . import api
-from ..models import User, Species, Population, Taxonomy, PlantTrait, Publication, Study, AuthorContact, AdditionalSource, Stage, StageType, Treatment, TreatmentType, MatrixStage, MatrixValue, Matrix, Interval, Bussy
+from ..models import User, Species, Population, Taxonomy, PlantTrait, Publication, Study, AuthorContact, AdditionalSource, Stage, StageType, Treatment, TreatmentType, MatrixStage, MatrixValue, Matrix, Interval, Fixed
 from ..decorators import admin_required, permission_required, crossdomain
 
 
@@ -9,7 +9,16 @@ def get_user(id):
     user = User.query.get_or_404(id)
     return jsonify(user.to_json())
 
-@api.route('/query/species')
+
+def fix_string(string):
+    capitalised = string.capitalize()
+    hypen_stripped = capitalised.replace("-", " ");
+
+    return hypen_stripped
+
+# Traversing via Species
+# All Species
+@api.route('/query/species/all')
 @crossdomain(origin='*')
 def get_all_species():
     all_species = Species.query.all()
@@ -21,29 +30,79 @@ def get_all_species():
     print species
     return jsonify(species)
 
-@api.route('/query/species-names')
-@crossdomain(origin='*')
-def get_all_species_names():
-    all_species = Species.query.all()
-    species = {'species' : []}
-    for s in all_species:
-        sp = s.to_json_simple()
-        species['species'].append(sp)
-
-    print species
-    return jsonify(species)
-
-@api.route('/query/species/<int:id>')
+# Species by ID (not useful for client, useful for testing for now)
+@api.route('/query/species/id=<int:id>')
 @crossdomain(origin='*')
 def get_species(id):
     species = Species.query.get_or_404(id)
     return jsonify(species.to_json())
 
-@api.route('/query/species-name/<name>')
+# Go with name for protocol
+@api.route('/query/species/name=<name>')
 @crossdomain(origin='*')
 def get_species_name(name):
+    name = fix_string(name)
     species = Species.query.filter_by(species_accepted=name).first()
     return jsonify(species.to_json())
+
+# Taxonomy of species
+@api.route('/query/species/name=<name>/taxonomy')
+@crossdomain(origin='*')
+def get_species_taxonomy(name):
+    name = fix_string(name)
+    species = Species.query.filter_by(species_accepted=name).first()
+    taxonomy = species.taxonomies[0]
+    return jsonify(taxonomy.to_json())
+
+@api.route('/query/species/name=<name>/plant-traits')
+@crossdomain(origin='*')
+def get_species_traits(name):
+    name = fix_string(name)
+    species = Species.query.filter_by(species_accepted=name).first()
+    traits = species.plant_traits[0]
+    return jsonify(traits.to_json())
+
+# Show all populations of Species
+@api.route('/query/species/name=<name>/populations')
+@crossdomain(origin='*')
+def get_species_populations(name):
+    name = fix_string(name)
+    species = Species.query.filter_by(species_accepted=name).first()
+    all_populations = species.populations
+    
+    populations = {"populations" : []}
+    for population in all_populations:
+        pop = population.to_json()
+        populations['populations'].append(pop)
+
+    return jsonify(populations)
+
+# Show all publications featuring this species
+@api.route('/query/species/name=<name>/publication')
+@crossdomain(origin='*')
+def get_species_publication(name):
+    name = fix_string(name)
+    species = Species.query.filter_by(species_accepted=name).first()
+    publication = species.taxonomies[0].publication
+    return jsonify(publication.to_json())
+
+# Show all matrices of this species
+@api.route('/query/species/name=<name>/matrices')
+@crossdomain(origin='*')
+def get_species_matrices(name):
+    name = fix_string(name)
+    species = Species.query.filter_by(species_accepted=name).first()
+    all_populations = species.populations
+    
+    matrices = {"matrices" : []}
+
+    for population in all_populations:
+        matrices_pop = population.study.matrices
+        for matrix in matrices_pop:
+            mat = matrix.to_json()
+            matrices['matrices'].append(mat)
+
+    return jsonify(matrices)
 
 @api.route('/query/taxonomy/<int:id>')
 @crossdomain(origin='*')
@@ -138,11 +197,11 @@ def get_interval(id):
     return jsonify(interval.to_json())
 
 # Not implemented yet
-@api.route('/query/bussy/<int:id>')
+@api.route('/query/fixed/<int:id>')
 @crossdomain(origin='*')
-def get_bussy(id):
-    bussy = Bussy.query.get_or_404(id)
-    return jsonify(bussy.to_json())
+def get_fixed(id):
+    fixed = Fixed.query.get_or_404(id)
+    return jsonify(fixed.to_json())
 
 @api.route('/query/population/<int:id>')
 @crossdomain(origin='*')
