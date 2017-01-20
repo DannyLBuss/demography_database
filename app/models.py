0571879be2ba594dd53ac3edf399f2fb79db7d9a
Francesca <f.sargent@exeter.ac.uk>
2017-01-20 15:25:52 +0000

    Started the editing of migration script in manage.py

--- app/models.py
+++ app/models.py
@@ -2396,6 +2396,9 @@ class Study(db.Model):
             }
         return study
 
+    def __getitem__(self, key):
+        return getattr(self, key)
+
     def __repr__(self):
         return '<Study %r>' % self.id
 
